968203e2fb0641855b004da072f32dd98c3a0ff8
Foulest <43710301+Foulest@users.noreply.github.com>
2024-08-27 14:32:57 -0700

    Converted to UtilityClass

--- src/main/java/net/foulest/kitpvp/util/ConstantUtil.java
+++ src/main/java/net/foulest/kitpvp/util/ConstantUtil.java
@@ -1,21 +1,20 @@
 package net.foulest.kitpvp.util;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class ConstantUtil {
+@UtilityClass
+public class ConstantUtil {
 
-    public static final String NO_PERMISSION = "&cNo permission.";
-    public static final String PLAYER_NOT_FOUND = "&cPlayer not found.";
-    public static final String COMMAND_DISABLED = "&cThat command is disabled.";
-    public static final String NOT_ENOUGH_COINS = "&cYou don't have enough coins.";
-    public static final String NOT_IN_SPAWN = "&cYou must be in spawn to use this command.";
-    public static final String IN_GAME_ONLY = "&cOnly players can execute this command.";
-    public static final String UNABLE_TO_REGISTER_TAB_COMPLETER = "Unable to register tab completer: ";
-    public static final String COMBAT_TAGGED = "&cYou may not use this command while combat tagged.";
-    public static final String NO_KIT_SELECTED = "&cYou do not have a kit selected.";
-    public static final String TELEPORTED_TO_SPAWN = "&aTeleported to spawn.";
-    public static final String NOT_ON_GROUND = "&cYou need to be on the ground.";
-    public static final String ABILITY_IN_SPAWN = "&cYou can't use your ability in spawn.";
+    public final String NO_PERMISSION = "&cNo permission.";
+    public final String PLAYER_NOT_FOUND = "&cPlayer not found.";
+    public final String COMMAND_DISABLED = "&cThat command is disabled.";
+    public final String NOT_ENOUGH_COINS = "&cYou don't have enough coins.";
+    public final String NOT_IN_SPAWN = "&cYou must be in spawn to use this command.";
+    public final String IN_GAME_ONLY = "&cOnly players can execute this command.";
+    public final String UNABLE_TO_REGISTER_TAB_COMPLETER = "Unable to register tab completer: ";
+    public final String COMBAT_TAGGED = "&cYou may not use this command while combat tagged.";
+    public final String NO_KIT_SELECTED = "&cYou do not have a kit selected.";
+    public final String TELEPORTED_TO_SPAWN = "&aTeleported to spawn.";
+    public final String NOT_ON_GROUND = "&cYou need to be on the ground.";
+    public final String ABILITY_IN_SPAWN = "&cYou can't use your ability in spawn.";
 }
